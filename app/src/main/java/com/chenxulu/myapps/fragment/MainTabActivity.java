2a085f7d096d1d6d827beb392b35c52b37ae5dd5
Xulu <godme001@gmail.com>
2018-02-07 10:14:20 +0800

    Revert: update

--- app/src/main/java/com/chenxulu/myapps/fragment/MainTabActivity.java
+++ app/src/main/java/com/chenxulu/myapps/fragment/MainTabActivity.java
@@ -45,22 +45,21 @@ public class MainTabActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_tab);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(3);
+        int count = mSectionsPagerAdapter.getCount();
+        if (count > 2) {
+            mViewPager.setCurrentItem(1);
+        }
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
 
     }
 
--- app/src/main/java/com/chenxulu/myapps/res/DrawableActivity.java
+++ app/src/main/java/com/chenxulu/myapps/res/DrawableActivity.java
@@ -7,9 +7,13 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.chenxulu.myapps.R;
 
+/**
+ * @author xulu
+ */
 public class DrawableActivity extends Activity {
     ImageView logoView;
 
@@ -17,11 +21,14 @@ public class DrawableActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawable_res);
-        logoView = (ImageView) findViewById(R.id.image_view);
+        logoView = findViewById(R.id.image_view);
+
+        final TextView textView = findViewById(R.id.text);
 
         findViewById(R.id.fab1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                textView.setText("width:" + logoView.getWidth() + "--" + "height:" + logoView.getHeight());
                 System.out.println("--");
                 System.out.println("width:" + logoView.getWidth());
                 System.out.println("height:" + logoView.getHeight());
