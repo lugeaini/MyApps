6f3f23ef34c3440f063deda028d2122325b5874a
Xulu <godme001@gmail.com>
2018-04-19 21:24:56 +0800

    update

--- app/src/main/java/com/chenxulu/myapps/ScrollingActivity.java
+++ app/src/main/java/com/chenxulu/myapps/ScrollingActivity.java
@@ -19,7 +19,7 @@ public class ScrollingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_scrolling);
+        setContentView(R.layout.activity_scrolling02);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
